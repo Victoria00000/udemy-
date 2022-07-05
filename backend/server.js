d6e5ef047525d5f2e51c42219ce6c219586bf2db
victoria <vvictoriaandres@gmail.com>
2022-07-05 11:21:57 -0300

    cors added + ./routes/user.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
 const express = require("express");
 
+const cors = require("cors");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//-para permitir un solo origen de consulta-
+//const options = {
+// origin: "http://localhost:3000",
+// useSuccessStatus: 200,
+//};
+// -y acá para permitir multi-origen de consulta-
+let allowed = ["http://localhost:3000", "http://localhost:5000"];
+const options = (req, res) => {
+  let temp;
+  let origin = req.header("Origin");
+  allowed.includes(origin)
+    ? (temp = { origin: true, useSuccessStatus: 200 })
+    : (temp = { origin: false });
+  res(null, temp);
+};
+app.use(cors(options)); //sin las options permite cualquier origen.
 
-app.get("/books", (req, res) => {
-  res.send("Hello books!!");
-});
+const { readdirSync } = require("fs");
+readdirSync("./routes").map( rt => app.use("/", require("./routes/" + rt)));
 
 app.listen(8000, () => {
   console.log("listening on http://localhost:8000");
